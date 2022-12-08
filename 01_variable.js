26c768bb0b05b2757794217fcf6abaf79c1d6684
reshmachinchakar <reshmadeshmane17@gmail.com>
2022-12-08 07:25:22 -0500

    01_variable declaration
    01_variable declaration

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+var myName;
+myName="Welcome In Javascript";
+console.log(myName);
+
+var myFullName;
+myFullName="Reshma Chinchakar";
+console.log(myFullName);
+
+var myLove;
+myLove="I Love Javascript";
+console.log(myLove);
+
+var fullName,age,address,pin_code,gender,is_married;
+fullName="Reshma Chinchakar";
+age="26";
+address="Pandharpur";
+pin_code="413104";
+gender="Female";
+is_married="Yes";
+console.log(fullName,age,address,pin_code,gender,is_married);
