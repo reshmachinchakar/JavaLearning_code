13b97048eeccdf31c8528517b8a977a637b65b60
reshmachinchakar <reshmadeshmane17@gmail.com>
2022-12-09 09:32:48 +0530

    swap values
    first assign to exchange the values of numbers

---
+++ 01_swap.js
@@ -0,0 +1,9 @@
+var myNumber,yourNumber,temp;
+myNumber=300;
+yourNumber=200;
+temp=myNumber;
+myNumber=yourNumber;
+yourNumber=temp;
+console.log(myNumber,yourNumber);
+
+
---
+++ 01_swap_ass01.js
@@ -0,0 +1,24 @@
+
+var num1,num2,num3, temp;
+
+console.log("======before swap========");
+num1=1000;
+console.log("num1=",num1);
+num2=2000;
+console.log("num2=",num2);
+num3=3000;
+console.log("num3=",num3);
+
+console.log("=========after swap========");
+
+temp=num1;
+num1=num2;
+console.log("num1=",num2);
+num2=num3;
+
+console.log("num2=",num3);
+num3=temp;
+console.log("num3=",temp);
+
+
+
