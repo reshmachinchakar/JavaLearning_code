445949c595ba75adf1576f9fd89f358e6167d6ba
reshmachinchakar <reshmadeshmane17@gmail.com>
2022-12-14 20:01:27 +0530

    string template first assignment
    string template first assignment

---
+++ 03_stringTemplateFirstAssign.js
@@ -0,0 +1,10 @@
+
+console.log(`1)My dream company is "TCS" `);
+
+var hobby1,hobby2,hobby3;
+hobby1="Listining Music";
+hobby2="Dancing";
+hobby3="Travelling";
+
+console.log(`Using Variable Substitution: ${hobby1},${hobby2},${hobby3}`);
+console.log("My hobbies are:",hobby1.concat(hobby2).concat(hobby3));
