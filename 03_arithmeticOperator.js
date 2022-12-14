ffb63034a03caa963b091ceabbe05b2371b32d0d
reshmachinchakar <reshmadeshmane17@gmail.com>
2022-12-14 20:02:57 +0530

    using arithmetic operator
    using arithmetic operator

---
+++ 03_arithmeticOperator.js
@@ -0,0 +1,27 @@
+console.log(`============ Unary Operators =================`);
+var num3 = 10;
+var result = ++num3; // num3 = num3 + 1;
+console.log(`Increment operator  result is ${result}`);
+
+var num4 = 5;
+var res = --num4; //num4 -1;
+console.log(`Decrement operator result is ${res}`);
+
+
+console.log(`============ Comparison Operators =================`);
+var num1 = 10;
+var num2 = 5;
+var num3 = 10
+var result = num1 > num2;
+console.log(`> Operator - Greater than ${result}`)
+console.log(`> Operator - Greater than ${num1>num3}`)
+
+var result = num1 < num2;
+console.log(`< Operator - Less than ${result}`)
+
+var result = num1 >= num3;
+console.log(`>= Operator - Greater than equal ==> ${result}`)
+
+var result = num1 <= num3;
+console.log(`<= Operator - Less than equal ==> ${result}`);
+
